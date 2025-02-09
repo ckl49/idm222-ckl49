cca6d209ee6f3e8e036aa5088ede52cd9eb2032b
kcaseylee <caklee2004@gmail.com>
2025-02-09 00:13:56 -0500

    continue building product
    messed up other pages but set more consistency within the product page, trying to reduce redundancy and make things more applicable

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// const summary = document.getElementById('mySummary');
+// const details = document.getElementById('myDetails');
+
+// details.addEventListener('toggle', function() {
+//     if (details.open) {
+//         summary.textContext = 'Less Info';
+//     } else {
+//         summary.textContent = 'More Info';
+//     }
+// } ) 
+
+// trying to tweak the details shown action when adding it in
